8d2b8809ad485644cbf42b0b50d07642e82df7d6
bhuvanesh-km <bhuvaneshkm441@gmail.com>
2025-10-17 19:46:59 +0530

    setup editor store

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,9 +2,56 @@
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import Table from "@tiptap/extension-table";
+import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+import TableRow from "@tiptap/extension-table-row";
+import TaskItem from "@tiptap/extension-task-item";
+import TaskList from "@tiptap/extension-task-list";
+
+import { useEditorStore } from "@/store/use-editor-store";
+
 export const Editor = () => {
+  const { setEditor } = useEditorStore();
+
   const editor = useEditor({
-    extensions: [StarterKit],
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
+    extensions: [
+      StarterKit,
+      Image,
+      ImageResize,
+      Table,
+      TableCell,
+      TableHeader,
+      TableRow,
+      TaskItem.configure({ nested: true }),
+      TaskList,
+    ],
     editorProps: {
       attributes: {
         style: "padding-left:56px; padding-right: 56px;",
@@ -12,7 +59,44 @@ export const Editor = () => {
           "focus:outline-none print:border-0 bg-white border border-[#c7c7c7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    content: "<p>Hello World! 🌎️</p>",
+    content: `
+      <h3>
+        Have you seen our tables? They are amazing!
+      </h3>
+      <ul>
+        <li>Tables with rows, cells and headers (optional)</li>
+        <li>Support for <code>colgroup</code> and <code>rowspan</code></li>
+        <li>And even resizable columns (optional)</li>
+      </ul>
+      <p>
+        Here is an example:
+      </p>
+      <table>
+        <tbody>
+          <tr>
+            <th colwidth="200">Name</th>
+            <th colspan="3" colwidth="150,100">Description</th>
+          </tr>
+          <tr>
+            <td>Cyndi Lauper</td>
+            <td>Singer</td>
+            <td>Songwriter</td>
+            <td>Actress</td>
+          </tr>
+          <tr>
+            <td>Marie Curie</td>
+            <td>Scientist</td>
+            <td>Chemist</td>
+            <td>Physicist</td>
+          </tr>
+          <tr>
+            <td>Indira Gandhi</td>
+            <td>Prime minister</td>
+            <td colspan="2">Politician</td>
+          </tr>
+        </tbody>
+      </table>
+    `,
     immediatelyRender: false,
   });
 
