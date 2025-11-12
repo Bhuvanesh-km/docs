0346d69d4d7116cb2565e27f2043acb03a8ab393
bhuvanesh-km <bhuvaneshkm441@gmail.com>
2025-11-12 22:22:22 +0530

    implement search docs

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,11 +5,13 @@ import { NavBar } from "./navbar";
 import { TemplatesGallery } from "./templates-gallery";
 import { api } from "../../../convex/_generated/api";
 import DocumentsTable from "./documents-table";
+import { useSearchParam } from "@/hooks/use-search-parm";
 
 const Home = () => {
+  const [search] = useSearchParam("search");
   const { results, status, loadMore } = usePaginatedQuery(
     api.documents.get,
-    { paginationOpts: {} },
+    { search },
     { initialNumItems: 5 },
   );
 
