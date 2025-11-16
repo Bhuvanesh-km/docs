538d75c684a72821fb4cea007dcca108a77915ce
Bhuvanesh-km <bhuvaneshkm441@gmail.com>
2025-11-16 11:59:18 +0530

    [Bhuvi] fix build error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { AlertCircle, ArrowLeftIcon, HomeIcon } from "lucide-react";
+import { AlertCircle } from "lucide-react";
 import Link from "next/link";
 
 const ErrorPage = ({
